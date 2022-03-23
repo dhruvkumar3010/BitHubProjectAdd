8c8ca04ae69f90d53425e967cad948c699e53bff
dhruvkumar3010 <pateldk3010@gmail.com>
2022-03-23 16:20:12 -0400

    add my first project

---
+++ src/com/java/sbi/cal.java
@@ -0,0 +1,17 @@
+package com.java.sbi;
+
+public class cal {
+
+	public int add(int i, int j) {
+		return i + j;
+
+	}
+
+	public static void main(String[] args) {
+
+		cal c = new cal();
+
+		System.out.println(c.add(5, 90));
+	}
+
+}
